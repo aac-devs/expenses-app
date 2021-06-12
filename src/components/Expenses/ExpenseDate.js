dc7a5352efe61fe330ad22a358e79068deb1ded2
Andrés <aac.devs@gmail.com>
2021-06-12 08:11:43 -0300

    Section 5 finished. Project finished

--- src/components/Expenses/ExpenseDate.js
+++ src/components/Expenses/ExpenseDate.js
@@ -1,8 +1,14 @@
 import "./ExpenseDate.css";
 
 const ExpenseDate = (props) => {
-  const month = props.date.toLocaleString("en-US", { month: "long" });
-  const day = props.date.toLocaleString("en-US", { day: "2-digit" });
+  const month = props.date.toLocaleDateString("en-US", {
+    month: "long",
+    timeZone: "UTC",
+  });
+  const day = props.date.toLocaleString("en-US", {
+    day: "2-digit",
+    timeZone: "UTC",
+  });
   const year = props.date.getFullYear();
   return (
     <div className="expense-date">
